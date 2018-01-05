3a01e3968aa6c290a5b09a28bb49a66bebf01a6f
Jolo Simeon <simeon.jolo@gmail.com>
2018-01-06 02:10:59 +0800

    ore wa

--- shaker.py
+++ shaker.py
@@ -13,18 +13,18 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('//is sana gay?'):
+    if message.content.startswith('//'):
         print(message.author.name + ' ' + str(message.timestamp))
+    if message.content.startswith('//is sana gay?'):
         await client.send_message(message.channel, 'yeah i am so gay', tts=True)
     if message.content.startswith('//aw'):
-        print(message.author.name + ' ' + str(message.timestamp))
         await client.send_message(message.channel, 'arvayne weak', tts=True)
     if message.content.startswith('//will'):
-        print(message.author.name + ' ' + str(message.timestamp))
         await client.send_message(message.channel, 'kamusta naman', tts=True)
     if message.content.startswith('//jay'):
-        print(message.author.name + ' ' + str(message.timestamp))
         await client.send_message(message.channel, 'jaylica likes girls', tts=True)
+    if message.content.startswith('//john'):
+        await client.send_message(message.channel, 'e show knee wa ow chin chin ow tah bey mas en ka?', tts=True)
     #    counter = 0
     #     tmp = await client.send_message(message.channel, 'Calculating messages...')
     #     async for log in client.logs_from(message.channel, limit=100):
