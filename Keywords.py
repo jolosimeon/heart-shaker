0f0fc55e160e522f6019c3943badd1af35820f43
simeon.jolo@gmail.com <simeon.jolo@gmail.com>
2018-01-22 22:19:27 +0800

    trial stored commands

--- Keywords.py
+++ Keywords.py
@@ -1,6 +1,86 @@
 import discord
 from discord.ext import commands
 
-class Keywords():
+import os
+from urllib import parse
+import psycopg2
+import re
+
+keywordList = {}
+cur = None
+forbidden = {'set', 'list', 'remove'}
+
+class Keywords:
     def __init__(self, bot):
         self.bot = bot
+        self.loadCommands()
+    
+    def initCon(self):
+        parse.uses_netloc.append("postgres")
+        url = parse.urlparse(os.environ["DATABASE_URL"])
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        global cur
+        cur = conn.cursor()
+    
+    def loadCommands(self):
+        self.initCon()
+        cur.execute("SELECT * FROM heartshaker.keywords;")
+        resultList = cur.fetchall()
+        global keywordList
+        keywordList = {}
+        for row in resultList:
+            keywordList[row[0]] = row[1]
+            
+
+    @commands.command()
+    async def set(self, word, *, value=None):
+        if value is None:
+            await self.bot.say("wat should I say")
+        elif word in forbidden:
+            await self.bot.say("cannot use that command")
+        else:
+            if (keywordList.get(word) is not None):
+                cur.execute("UPDATE heartshaker.keywords "
+                            "SET value = %s"
+                            "WHERE keyword = %s", (value, word)) 
+            else:
+                cur.execute("INSERT INTO heartshaker.keywords "
+                            "VALUES (%s, %s)", (word, value))
+            keywordList[word] = value
+
+    @commands.command()
+    async def view(self):
+        viewList = "**Commands:** \n"
+        for key, value in keywordList:
+            viewList += "```" + str(key) + ":``` " + str(value) + "\n"
+        await self.bot.say(viewList)
+    
+    @commands.command()
+    async def remove(self, word):
+        if word is None:
+            await self.bot.say("nothing to remove")
+        elif word in forbidden:
+            await self.bot.say("cannot remove dat")
+        elif (keywordList.get(word) is None):
+            await self.bot.say(word + " does not exist")
+        else:
+            cur.execute("DELETE FROM heartshaker.keywords "
+                        "WHERE keyword = %s", word)
+            keywordList.pop(word)
+
+    async def on_message(self, msg):
+        if msg.content.startswith('//'):
+            word = msg
+            re.sub('//','', word, 1)
+            if (keywordList.get(word) is not None):
+                await self.bot.send_message(msg.channel, keywordList[msg], tts=True)
+        await self.bot.process_commands(msg)
+
+def setup(bot):
+    bot.add_cog(Keywords(bot))
--- likey.py
+++ likey.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 import os
-
+from random import randint 
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="//", description="you're my heart shaker shaker")
@@ -12,11 +12,19 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    bot.add_cog(Keywords(bot))
+    bot.load_extension("Keywords")
     await bot.change_presence(game=discord.Game(name='TWICE - Heart Shaker'))
+
+async def status_loop():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        playing = ["TWICE - Heart Shaker", "TWICE - Likey", "Overwatch", "Fortnite", "League of Legends"]
+        await bot.change_presence(game=discord.Game(name=playing[randint(0, 4)]))
+        await asyncio.sleep(60)
 
 #async def on_message(message):
     ##await bot.send_message(message.channel, 'sollenda me likey likey', tts=True)
 
 ##bot.run(os.getenv('TOKEN'))
+bot.loop.create_task(status_loop())
 bot.run(os.getenv('DEV-TOKEN'))
