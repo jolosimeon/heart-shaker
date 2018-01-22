711eaafd8c87722629f8ddcc4c15cf9ea19a5d3d
simeon.jolo@gmail.com <simeon.jolo@gmail.com>
2018-01-22 14:01:00 +0800

    try new syntax

--- likey.py
+++ likey.py
@@ -12,6 +12,7 @@ bot = commands.Bot(command_prefix="//", description="you're my heart shaker shak
 @bot.event
 async def on_ready():
     print("I'm ready!")
+    bot.send_message('sollenda me likey likey', tts=True)
 
 ##bot.run(os.getenv('TOKEN'))
 bot.run(os.getenv('DEV-TOKEN'))
