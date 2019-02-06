72aac11bbfe3608e39183be4312ef5e6a8104861
simeon.jolo@gmail.com <simeon.jolo@gmail.com>
2019-02-06 09:59:41 +0800

    Added libraries

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
+const express = require('express');
+const { Client } = require('pg');
+const port = process.env.PORT || 3003;
 
 var app = express();
 
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World heart-shaker'}));
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+app.get('/', (req, res) => {
+ res.send(JSON.stringify({ Hello: 'heart-shaker'}));
+});
+
+app.get('/user', (req, res) => {
+
+});
+
+app.get('/user/:id', (req, res) => {
+
 });
 
 app.listen(port, function () {
- console.log('Example app listening on port !');
+ console.log('App listening on port '+ port);
 });
